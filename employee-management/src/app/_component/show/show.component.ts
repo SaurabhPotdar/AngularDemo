c5cb4bda216d892ef6e12c2ececd6f9b810ed285
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2021-04-06 18:15:16 +0530

    Update show.component.ts

--- employee-management/src/app/_component/show/show.component.ts
+++ employee-management/src/app/_component/show/show.component.ts
@@ -16,8 +16,8 @@ export class ShowComponent implements OnInit {
   employees: Employee[] = [];
 
   ngOnInit() {
-    this.service.getAllData().subscribe((data: Employee[]) => { this.employees = data; console.log(this.employees); }, error => { alert(error.error);
-    console.log(error.error);
+    this.service.getAllData().subscribe((data: Employee[]) => { this.employees = data; }, error => {
+      alert(error.error);
     });
   }
 
