165b136672835c4dd0e93c74665c0f2ee9650cac
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2021-04-06 15:25:39 +0530

    Added css

--- employee-management/src/app/app-routing.module.ts
+++ employee-management/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Error404Component } from './_component/error404/error404.component';
 import { ShowComponent } from './_component/show/show.component';
+import { TempComponent } from './_component/temp/temp.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'show', pathMatch: 'full' },
   { path: 'show', component: ShowComponent },
+  { path: 'temp', component: TempComponent },
   { path: '**', component: Error404Component }
 ];
 
--- employee-management/src/app/app.module.ts
+++ employee-management/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './_component/error404/error404.component';
 import { ShowComponent } from './_component/show/show.component';
+import { TempComponent } from './_component/temp/temp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowComponent,
-    Error404Component
+    Error404Component,
+    TempComponent
   ],
   imports: [
     BrowserModule,
